a49851e3deeb6ce4c0f7debce69d6f38ffd49c06
Sai Motukuri <ssm48892@gmail.com>
2022-10-25 17:46:50 -0500

    Add files via upload

---
+++ xv6/kernel/syscall.d
@@ -0,0 +1,4 @@
+kernel/syscall.d kernel/syscall.o: kernel/syscall.c include/types.h \
+ kernel/defs.h include/param.h kernel/mmu.h kernel/proc.h include/pstat.h \
+ include/param.h kernel/spinlock.h include/x86.h include/syscall.h \
+ kernel/sysfunc.h
